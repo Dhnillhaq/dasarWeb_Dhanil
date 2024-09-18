e06c0664597c4fc5e899318a7569127c8a85957a
Dhnillhaq <yakulhaq@gmail.com>
2024-09-18 07:50:32 +0700

    feat : Praktikum 2

--- Praktikum_Week_4/tipe_data.php
+++ Praktikum_Week_4/tipe_data.php
@@ -17,9 +17,32 @@ $nilaiMatematika = 5.1;
 $nilaiIPA = 6.7;
 $nilaiBahasaIndonesia = 9.3;
 $rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
 echo "Matematika: {$nilaiMatematika} <br>";
 echo "IPA: {$nilaiIPA} <br>";
 echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
-echo "Rata-rata: {$rataRata} <br>";
+echo "Rata-rata: {$rataRata} <br><br>";
+
 var_dump($rataRata);
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+echo "<br><br>";
+var_dump($apakahSiswaLulus);
+echo "<br><br>";
+var_dump($apakahSiswaSudahUjian);
+
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . "" . $namaBelakang;
+
+echo "<br><br>Nama Depan: {$namaDepan} <br><br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br><br>';
+echo $namaLengkap;
+
+$listMahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lendis Fabri"];
+echo "<br><br>".$listMahasiswa[0];
 ?>

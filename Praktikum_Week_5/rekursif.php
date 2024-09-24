d98cbd34c0ede10d4197f8daf24435223673633e
Dhnillhaq <yakulhaq@gmail.com>
2024-09-24 10:21:27 +0700

    feat : Praktikum 2

--- Praktikum_Week_5/rekursif.php
+++ Praktikum_Week_5/rekursif.php
@@ -1,3 +1,70 @@
-<?php 
-echo "<h2>Cihuy</h2>"
+<?php
+function tampilkanHaloDunia()
+{
+    echo "Halo dunia!<br>";
+
+    tampilkanHaloDunia();
+}
+
+// tampilkanHaloDunia();
+
+function tampilkanAngka(int $jumlah, int $indeks = 1)
+{
+    echo "Perulangan ke-{$indeks}<br>";
+
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
+
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
+
+echo "<hr>";
+function tampilkanMenuBertingkat(array $menu)
+{
+    echo "<ul>";
+    foreach ($menu as $key) {
+        echo "<li>{$key['nama']}";
+        if (isset($key['subMenu'])) {
+            tampilkanMenuBertingkat($key['subMenu']);
+        }
+        echo "</li>";
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
 ?>

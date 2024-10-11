8491231dd3f2b12f875405a10a1ef71bb415cc3a
Dhnillhaq <yakulhaq@gmail.com>
2024-10-11 19:34:38 +0700

    feat : praktikum7 for process validating input

---
+++ Praktikum_Week_7/proses_validasi.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama = $_POST["nama"];
+    $password = $_POST["pw"];
+    $email = $_POST["email"];
+    $errors = array();
+
+    // Validasi Nama
+    if (empty($nama)) {
+        $errors[] = "Nama harus diisi.";
+    }
+
+    if (empty($password)) {
+        $errors[] = "Password harus diisi.";
+    } elseif (strlen($password) < 8) {
+        $errors[] = "Password harus terdiri dari minimal 8 karakter.";
+    }
+
+    // Validasi Email
+    if (empty($email)) {
+        $errors[] = "Email harus diisi.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Format email tidak valid.";
+    }
+
+    // Jika ada kesalahan validasi
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    } else {
+        // Lanjutkan dengan pemrosesan data jika semua validasi berhasil
+        // Misalnya, menyimpan data ke database atau mengirim email
+        echo "Data berhasil dikirim: Nama = $nama, Password = $password, Email = $email";
+    }
+}
+?>

c9730a93a468761ea630a377501a39c277064c3b
Dhnillhaq <yakulhaq@gmail.com>
2024-10-17 07:26:04 +0700

    feat : praktikum 7 for implement cookies to cart shop feature

---
+++ Praktikum_Week_8/keranjangBelanja.php
@@ -0,0 +1,15 @@
+<html>
+
+<head></head>
+
+<body>
+    <?php
+    $beliNovel = $_COOKIE['beliNovel'];
+    $beliBuku = $_COOKIE['beliBuku'];
+    echo "Jumlah Novel: " . $beliNovel . "<br>";
+    echo "Jumlah Buku: " . $beliBuku;
+    ?>
+    
+</body>
+
+</html>
